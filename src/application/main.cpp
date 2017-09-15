47f39277917c0ed0506e1181a670d16e6721cbf5
Damian Gzik <damian.gzik@outlook.com>
2017-09-15 20:12:22 +0200

    some comments added

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -1,3 +1,8 @@
+//Author: Damian Gzik
+//Simple barebone TCPServer on WinSocket
+//multiClient and multitask ready with thread's 
+//[windows support, MSDN std foo used]
+
 #include <iostream>
 #include <conio.h>
 #include "../server/TCPServer.h"
@@ -8,7 +13,7 @@ void RequestHandler(Ortosoft::Software::TCPServer* server, int client, std::stri
 
 int main(int argc, char* argv[])
 {
-	Ortosoft::Software::TCPServer myServer("192.168.2.105", 123, RequestHandler);
+	Ortosoft::Software::TCPServer myServer("127.0.0.1", 123, RequestHandler);	//loopback to the owner Ip, or better put here your IpV4 addres,port
 	
 	if (myServer.Init())
 	{
@@ -25,6 +30,10 @@ int main(int argc, char* argv[])
 
 void RequestHandler(Ortosoft::Software::TCPServer* server, int client, std::string querry)
 {
+	//[TCPServer* server] object can perform some actions only here, not in new thred
+	//f.e. send pending status to the client: server->Send(client, "querry is being processing" );
+	//!!! if [TCPServer* server] object used in ne thread - can cause undefined behavior
+
 	std::thread sendT([=]() {
 
 		//send querry to database 
@@ -32,7 +41,8 @@ void RequestHandler(Ortosoft::Software::TCPServer* server, int client, std::stri
 		//valid response ? send it back to the client : send zero string
 
 		//only for the tests
-		if (querry == "Admin,123123")
+		//i.e. if querry == "GET /index.html HTTP/1.1 {load index.html as std::string, send it back}"
+		if (querry == "Admin,123123")	//test querry
 		{
 			std::string response = "everest";
 			int status = send(client, response.c_str(), response.size() + 1, 0);
